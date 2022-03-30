6a00fb5698eac1f87448e77aa2b85a4b101ba8d0
yhkcjo <yhkcjo@gmail.com>
2022-03-30 17:28:13 +0800

    新增陣列旋轉 InPlace 方法

--- QuestionTests/QuestionTests/TwoDimensionalArrayQuestions/Rotating2DArrayTest.cs
+++ QuestionTests/QuestionTests/TwoDimensionalArrayQuestions/Rotating2DArrayTest.cs
@@ -20,6 +20,11 @@ namespace QuestionTests.TwoDimensionalArrayQuestions
 
             Assert.AreEqual(
                 new int[,] { { 7, 4, 1 }, { 8, 5, 2 }, { 9, 6, 3 } }, rotating2DArray.Rotating(array));
+
+            array = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            Assert.AreEqual(
+                new int[,] { { 7, 4, 1 }, { 8, 5, 2 }, { 9, 6, 3 } }, rotating2DArray.RotatingInPlace(array));
         }
     }
 }
--- TwoDimensionalArrayQuestions/TwoDimensionalArrayQuestions/Rotating2DArray.cs
+++ TwoDimensionalArrayQuestions/TwoDimensionalArrayQuestions/Rotating2DArray.cs
@@ -28,5 +28,66 @@ namespace TwoDimensionalArrayQuestions
 
             return res;
         }
+
+        public int[,] RotatingInPlace(int[,] originArray)
+        {
+            var level = originArray.GetLength(0);
+
+            // row
+            var ceiling = Convert.ToInt32(Math.Ceiling(Convert.ToDecimal(level) / 2));
+
+            // col
+            var floor = Convert.ToInt32(Math.Floor(Convert.ToDecimal(level) / 2));
+
+            int indexA = 0;
+            int indexB = 0;
+            int[] temp = new int[4];
+            for (int row = 0; row < ceiling; row += 1) 
+            {
+                for(int col = 0; col < floor; col += 1) 
+                {
+                    // 重置站存陣列，存放轉移值
+                    temp = new int[4] { -1, -1, -1, -1 };
+
+                    indexA = row;
+                    indexB = col;
+
+                    // 將舊值存起來
+                    for(int i = 0; i < 4; i += 1) 
+                    {
+                        temp[i] = originArray[indexA, indexB];
+                        var newPosition = this.GetNewPosition(indexA, indexB,level);
+                        indexA = newPosition[0];
+                        indexB = newPosition[1];
+                    }
+
+                    // 將暫存值回填到對應位置
+                    for (int i = 0; i < 4; i += 1)
+                    {
+                        originArray[indexA, indexB] = temp[(i + 3) % 4];
+                        var newPosition = this.GetNewPosition(indexA, indexB, level);
+                        indexA = newPosition[0];
+                        indexB = newPosition[1];
+                    }
+
+                }
+            }
+
+            return originArray;
+        }
+
+        /// <summary>
+        /// 公式
+        /// 新 row = 舊 col
+        /// 新 col = 陣列長 - 1 - 舊 row
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        private int[] GetNewPosition(int row,int col,int level) 
+        {
+            return new int[] { col, level - 1 - row };
+        }
     }
 }
