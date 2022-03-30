1423355c4aeb6f86d656e25d9c54d321c0cb0159
yhkcjo <yhkcjo@gmail.com>
2022-03-30 16:05:02 +0800

    新增二維陣列旋轉 90 度問題

---
+++ QuestionTests/QuestionTests/TwoDimensionalArrayQuestions/Rotating2DArrayTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwoDimensionalArrayQuestions;
+
+namespace QuestionTests.TwoDimensionalArrayQuestions
+{
+    [TestFixture]
+    public class Rotating2DArrayTest
+    {
+        [Test]
+        public void Test() 
+        {
+            Rotating2DArray rotating2DArray = new Rotating2DArray();
+
+            var array = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            Assert.AreEqual(
+                new int[,] { { 7, 4, 1 }, { 8, 5, 2 }, { 9, 6, 3 } }, rotating2DArray.Rotating(array));
+        }
+    }
+}
---
+++ TwoDimensionalArrayQuestions/TwoDimensionalArrayQuestions/Rotating2DArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDimensionalArrayQuestions
+{
+    /// <summary>
+    /// 旋轉 2 維陣列(90 度)
+    /// </summary>
+    public class Rotating2DArray
+    {
+        public int[,] Rotating(int[,] originArray) 
+        {
+            var res = new int[originArray.GetLength(0), originArray.GetLength(1)];
+
+            // 公式
+            // 新 i = 舊 j
+            // 新 j = 陣列長 - 1 - 舊 i
+            for(int i =0;i< originArray.GetLength(0); i += 1) 
+            {
+                for(int j = 0; j < originArray.GetLength(1); j += 1) 
+                {
+                    res[j, originArray.GetLength(1) - 1 -i] = originArray[i,j];
+                }
+            }
+
+            return res;
+        }
+    }
+}
